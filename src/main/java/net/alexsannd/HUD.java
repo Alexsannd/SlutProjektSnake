53800f21a9c8afdbf2693fe859dd4ae6262bc98c
Alexsannd <alexsandersannestedt@gmail.com>
2024-02-29 17:22:59 +0100

    Added a HUD and removed finished TODOs. Removed some unused black space on the right of the screen.

---
+++ src/main/java/net/alexsannd/HUD.java
@@ -0,0 +1,45 @@
+package net.alexsannd;
+
+import java.awt.*;
+
+public class HUD {
+    private int score;
+    private int highScore;
+    private int level;
+    private int width;
+    private int height;
+    private int x;
+    private int y;
+
+    public HUD(int x, int y, int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.score = 0;
+        this.highScore = 0;
+        this.level = 1;
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(new Color(50, 50, 50));
+        g.fillRect(x, y, width, height);
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 20));
+        g.drawString("Score: " + score, x + 10, y + 20);
+        g.drawString("High Score: " + highScore, x + 10, y + 40);
+        g.drawString("Level: " + level, x + 10, y + 60);
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setHighScore(int highScore) {
+        this.highScore = highScore;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
--- src/main/java/net/alexsannd/SnakeGame.java
+++ src/main/java/net/alexsannd/SnakeGame.java
@@ -19,11 +19,10 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     boolean running = false;
     double time = 0;
     int timestep = 1000;
-    //TODO 1
-    //Lägg till variabler för storleken på ditt rutnät. Antalet rutor i x- och y-led
-    // storleken på rutorna i pixlar, storleken på mellanrummet mellan rutorna i pixlar
-    //TODO 2
-    //Ändra så att det blir en matematiskt uträkning som räknar ut hur brett och högt fönstret behöver vara nedan
+
+    int cellWidth = 30, cellHeight = 30, Gap = 3;
+
+    HUD hud;
     int WIDTH = 1000, HEIGHT = 800;
     JFrame window;
     Timer tm = new Timer(timestep, this);
@@ -47,18 +46,17 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         setFocusable(true);
         requestFocus();
         this.window = window;
-        gridController = new GridController(WIDTH/(30+3), HEIGHT/(30+3), 30, 30, 3, Color.WHITE);
+        gridController = new GridController(WIDTH/(cellWidth+Gap), HEIGHT/(cellHeight+Gap)-3, cellWidth, cellHeight, Gap, Color.WHITE);
+        WIDTH = gridController.getWidth();
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
         snake = new Snake(gridController);
         snake.setHead(gridController.getRows()/2, gridController.getColumns()/2);
         snake.addBodyPart();
         snake.addBodyPart();
+        hud = new HUD(20, gridController.getHeight()+20, WIDTH-40, HEIGHT - gridController.getHeight()-40);
     }
 
     public void setUp(){
-        //TODO 4
-        //Lägg till bitar till ArrayListan snake som ska representera vart på spelplanen du vill att ormen ska börja.
-        //Använd samma numrering på Coordinates xPos och yPos som du använde dig av när du ritade ut rutnätet.
-        //Skapa även ett äpple och bestäm vart det första äpplet ska bli placerat i början av spelet.
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
@@ -74,16 +72,9 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     protected void paintComponent(Graphics g){
         g.setColor(Color.BLACK);
         g.fillRect(0,0, WIDTH, HEIGHT);
-        //TODO 3
-        //Välj en färg på ditt rutnät genom g.setColor, använd sedan två nästlade for.loopar för att rita ut ett rutnät
-        //på skärmen. Se dokumentet "Utritande av rutnät" på classroom.
 
-        //TODO 5
-        //Välj en färg för ormen och rita sedan ut hela ormen på skärmen, detta gör du genom att skapa en for-loop som
-        //går igenom ArrayListan snake och ritar ut en ruta med samma formel som för rutnätet, men istället för i och j
-        //så använer du dig av varje xPos och yPos för ormens delar. Du kommer märka att du har gjort rätt om ormens
-        // bitar ligger precis på ditt rutnäts bitar.
         gridController.paint(g);
+        hud.paint(g);
     }
 
     @Override
@@ -99,14 +90,6 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
             snake.move(direction);
 
         }
-
-        //TODO 6
-        //Med hjälp av en for-loop som börjar i slutet av ArrayListan och rör sig bakåt (mot i = 0) så vill vi gå
-        // igenom ArrayListan och flytta varje bit av ormen så det ser ut som att den rör på sig. När varje bit av
-        // svansen (allt som inte är ett huvud). Har flyttat på sig så är det dags att flytta på huvudet. För att göra
-        // det så behöver vi ha en variabel som håller koll på åt vilket håll huvudet ska röra sig. Jag rekommednerar
-        // att använda en int och låta 1,2,3 och 4 representera de fyra riktningarna som den kan röra sig åt.
-        // Efter for-loopen, gör fyra if-satser som flyttar huvudet åt rätt håll beroende på värdet på variabeln.
 
         //TODO 7
         //Efter att alla bitar av ormen har rört på sig så behöver du kontrollera om ormen har hamnat utanför spelplanen
